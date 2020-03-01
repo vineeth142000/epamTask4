7d58e0d7a2a4230eec5980c1b2cbe5fd111ad259
vineeth142000 <varmavineeth4@gmail.com>
2020-03-01 10:24:34 +0530

    first commit

---
+++ task4epam/src/test/java/task4epam/calciiTest.java
@@ -0,0 +1,14 @@
+package task4epam;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class calciiTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
